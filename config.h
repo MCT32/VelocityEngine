8a5017ac5517fa0b0c75563202965945bb190272
MCT32 <ivanhen99@gmail.com>
2023-04-07 18:06:31 +1000

    increased range

--- config.h
+++ config.h
@@ -1,6 +1,6 @@
 #define FOV 1 // Defines the width of the camera plane
 
-#define RANGE 8 // Defines how far rays are allowed to travel to find intersections
+#define RANGE 20 // Defines how far rays are allowed to travel to find intersections
 
 #define SENSITIVITY 1
 
