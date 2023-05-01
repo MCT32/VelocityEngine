090b6d84b66b43f6e114e757632449b134ccd587
MCT32 <ivanhen99@gmail.com>
2023-05-01 14:45:47 +1000

    fixed inconsistancy in tab length

--- logging.cpp
+++ logging.cpp
@@ -4,16 +4,16 @@ std::ofstream log_file;
 
 void init_log()
 {
-	log_file.open("log.txt", std::fstream::out);
+  log_file.open("log.txt", std::fstream::out);
 }
 
 void end_log()
 {
-	log_file.close();
+  log_file.close();
 }
 
 void log(log_level const level, std::string_view const message)
 {
-	std::cout << "[" << static_cast<char>(level) << "] " << message << "\n";
-	log_file << "[" << static_cast<char>(level) << "] " << message << "\n";
+  std::cout << "[" << static_cast<char>(level) << "] " << message << "\n";
+  log_file << "[" << static_cast<char>(level) << "] " << message << "\n";
 }
