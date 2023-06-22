0605dfa471711af3199436d1de5dcf24466da9fb
MCT32 <ivanhen99@gmail.com>
2023-06-22 15:57:39 +1000

    refactored config.h

--- src/config.h
+++ src/config.h
@@ -1,17 +1,26 @@
-#define FOV 1 // Defines the width of the camera plane
-#define RANGE 20 // Defines how far rays are allowed to travel to find intersections
-#define SENSITIVITY 1
+// Constants
+#define PI 3.14159			// Constant defining pi
 
-#define PI 3.14159
 
-#define LOG_INFO 1
+// User settings
+#define FOV 1				// Width of the camera plane
+#define RANGE 20			// Distance rays are allowed to travel to find intersections
+#define SENSITIVITY 1			// Sensitivity of mouse movement
 
-#define MAP_NAME "test_room.gm"
 
-#define MAX_SPEED 2
-#define MAX_ACCEL MAX_SPEED * 10
+// Debug settings
+#define LOG_INFO 1			// Level of log messages that should be recorded
 
-#define FRICTION 5
-#define STOPSPEED 0.2
 
-#define TEXTURE_STRING_LENGTH 32
+#define MAP_NAME "test_room.gm"		// Map to load
+
+
+// Movement
+#define MAX_SPEED 2			// Maximum speed player is able to walk
+#define MAX_ACCEL MAX_SPEED * 10	// Speed player is able to accelerate to reach max speed
+
+#define FRICTION 5			// Rate player slows to a stop
+#define STOPSPEED 0.2			// Speed player should be considered stationary
+
+
+#define TEXTURE_STRING_LENGTH 32	// Maximum length of a texture name
